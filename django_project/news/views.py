1ea2393572bf2a6eb446cacb83029619eb1005fb
Pavel Khramov <paul.khramov@gmail.com>
2022-10-10 19:54:26 +0600

    feature: 'update page' added

--- django_project/news/views.py
+++ django_project/news/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from .models import Articles
 from .forms import ArticlesForm
-from django.views.generic import DetailView
+from django.views.generic import DetailView, UpdateView
 
 
 def news_home(request):
@@ -12,6 +12,12 @@ class NewsDetailView(DetailView):
     model = Articles
     template_name = 'news/article_view.html'
     context_object_name = 'article'
+
+class NewsUpdateView(UpdateView):
+    model = Articles
+    template_name = 'news/create.html'
+
+    form_class = ArticlesForm
 
 def create(request):
     error = ''
